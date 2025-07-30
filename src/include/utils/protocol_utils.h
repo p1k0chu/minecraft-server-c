8930c66746e82670dc7378dc737c0887d03dc122
p1k0chu <anatoliyid0207@gmail.com>
2025-07-30 22:28:48 +0300

    style: reformat protocol_utils.h

--- src/include/utils/protocol_utils.h
+++ src/include/utils/protocol_utils.h
@@ -2,8 +2,8 @@
 
 #include <sys/types.h>
 
-void write_prefixed_bytes(char *dst, const char *src, int n, uint *out_written_bytes);
+void  write_prefixed_bytes(char *dst, const char *src, int n, uint *out_written_bytes);
 void *read_prefixed_bytes(const char *src, uint *out_consumed_bytes);
-void send_var_int(int sockfd, int value);
-int  recv_var_int(int sockfd);
+void  send_var_int(int sockfd, int value);
+int   recv_var_int(int sockfd);
 
