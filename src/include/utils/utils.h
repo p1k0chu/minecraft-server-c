37e1be93a66d248f216932e68459028be1226f71
p1k0chu <anatoliyid0207@gmail.com>
2025-08-01 15:02:20 +0300

    util macro to check condition
    like in kotlin :3

--- src/include/utils/utils.h
+++ src/include/utils/utils.h
@@ -1,4 +1,12 @@
 #pragma once
 
+#define check(condition, error_str) \
+    {                               \
+        if (!(condition)) {          \
+            fprintf(stderr, (error_str));\
+            exit(1);\
+        }                           \
+    }
+
 void error(const char *msg);
 
